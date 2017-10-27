2458e6859c0c688048253166105ce58d173f1fb4
James McKay <code@jamesmckay.net>
2017-10-27 09:13:39 +0100

    Version 0.1.0 for initial release

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     install_requires=[],
     zip_safe=False,
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
     ],
